8c3354309b334a24ef5ae318cae75ec8e00de294
amit-g0swami <goswamiempire@gmail.com>
2022-07-07 10:11:41 +0530

    setup sanity

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {
+    BellIcon, HashtagIcon, BookmarkIcon, CollectionIcon, DotsCircleHorizontalIcon, MailIcon, UsersIcon, HomeIcon
+} from "@heroicons/react/outline";
+import SidebarRow from './SidebarRow';
 
 function Sidebar() {
     return (
-        <div>Sidebar</div>
+        <div className='col-span-2 flex flex-col items-center px-4 md:items-start'><img className='m-3 h-10 w-10' src="https://links.papareact.com/drq" alt="" />
+            <SidebarRow Icon={HomeIcon} title="Home" />
+            <SidebarRow Icon={BellIcon} title="Notifications" />
+            <SidebarRow Icon={HashtagIcon} title="Explore" />
+            <SidebarRow Icon={CollectionIcon} title="Lists" />
+            <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+            <SidebarRow Icon={MailIcon} title="Messages" />
+
+            <SidebarRow Icon={UsersIcon} title="Sign In" />
+            <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
+        </div>
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="mx-auto max-h-screen overflow-hidden lg:max-w-6xl">
       <Head>
         <title>twitter clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className="grid grid-cols-9">
         <Sidebar />
+        <Feed />
+        <Widgets />
       </main>
     </div>
   )
