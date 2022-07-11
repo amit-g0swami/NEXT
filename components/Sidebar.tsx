2f7a7ea35d76f8fe86a1d770cc453ada7a07c2ae
amit-g0swami <goswamiempire@gmail.com>
2022-07-11 10:31:29 +0530

    next-auth

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,8 +3,10 @@ import {
     BellIcon, HashtagIcon, BookmarkIcon, CollectionIcon, DotsCircleHorizontalIcon, MailIcon, UsersIcon, HomeIcon
 } from "@heroicons/react/outline";
 import SidebarRow from './SidebarRow';
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 function Sidebar() {
+    const { data: session } = useSession()
     return (
         <div className='col-span-2 flex flex-col items-center px-4 md:items-start'><img className='m-3 h-10 w-10' src="https://links.papareact.com/drq" alt="" />
             <SidebarRow Icon={HomeIcon} title="Home" />
@@ -14,7 +16,11 @@ function Sidebar() {
             <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
             <SidebarRow Icon={MailIcon} title="Messages" />
 
-            <SidebarRow Icon={UsersIcon} title="Sign In" />
+            <SidebarRow
+                onClick={session ? signOut : signIn}
+                Icon={UsersIcon}
+                title={session ? 'Sign Out' : 'Sign In'}
+            />
             <SidebarRow Icon={DotsCircleHorizontalIcon} title="More" />
         </div>
     )
